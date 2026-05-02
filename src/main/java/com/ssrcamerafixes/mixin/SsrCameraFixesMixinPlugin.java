be19e450456d8f8409cf3e3c396665214a9677c9
Seramicx <125859300+Seramicx@users.noreply.github.com>
2026-05-01 23:29:58 -0700

    Add MixinPlugin to safely skip EF mixins when unloaded

---
+++ src/main/java/com/ssrcamerafixes/mixin/SsrCameraFixesMixinPlugin.java
@@ -0,0 +1,50 @@
+package com.ssrcamerafixes.mixin;
+
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import java.util.List;
+import java.util.Set;
+
+public class SsrCameraFixesMixinPlugin implements IMixinConfigPlugin {
+
+    private boolean hasEpicFight = false;
+
+    @Override
+    public void onLoad(String mixinPackage) {
+        try {
+            Class.forName("yesman.epicfight.api.client.camera.EpicFightCameraAPI", false, this.getClass().getClassLoader());
+            hasEpicFight = true;
+        } catch (ClassNotFoundException e) {
+            hasEpicFight = false;
+        }
+    }
+
+    @Override
+    public String getRefMapperConfig() {
+        return null;
+    }
+
+    @Override
+    public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
+        if (mixinClassName.contains("MixinDisableEpicFightSsrLockOnTick") || mixinClassName.contains("MixinForceSsrOffsetDuringLockOn")) {
+            return hasEpicFight;
+        }
+        return true;
+    }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {}
+
+    @Override
+    public List<String> getMixins() {
+        return null;
+    }
+
+    @Override
+    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {}
+
+    @Override
+    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {}
+}
